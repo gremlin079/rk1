0d85bde8ba2db321ea410735b54d7df4368f32d1
gremlin079 <sashdmitriev2006@gmail.com>
2025-05-19 15:44:24 +0300

    Update concreteObserver.h

--- include/concreteObserver.h
+++ include/concreteObserver.h
@@ -1,1 +1,19 @@
+#ifndef CONCRETE_OBSERVER_H
+#define CONCRETE_OBSERVER_H
 
+#include "Observer.h"
+#include "WeatherData.h"
+#include <string>
+
+class ConcreteObserver : public Observer {
+private:
+    std::string name;
+    WeatherData* subject;
+
+public:
+    ConcreteObserver(const std::string& name, WeatherData* subject);
+    void update() override;
+    std::string getName() const;
+};
+
+#endif // CONCRETE_OBSERVER_H
