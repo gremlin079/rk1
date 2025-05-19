72e2d4f37aef5e6b83410f0c38a791695f63d254
gremlin079 <sashdmitriev2006@gmail.com>
2025-05-19 15:07:47 +0300

    Update test_weatherData.cpp

--- tests/test_weatherData.cpp
+++ tests/test_weatherData.cpp
@@ -1,1 +1,13 @@
+#include <gtest/gtest.h>
+#include "../include/weatherData.h"
 
+TEST(WeatherDataTest, Inheritance) {
+    EXPECT_TRUE(std::is_base_of<Subject, WeatherData>::value);
+}
+
+TEST(WeatherDataTest, MethodsCallable) {
+    WeatherData wd;
+    Observer obs;
+    EXPECT_NO_THROW(wd.registerObserver(obs));
+    EXPECT_NO_THROW(wd.notifyObservers()));
+}
