c691fb8d8e93240935b9a9f55b330c423fb9cd4e
gremlin079 <sashdmitriev2006@gmail.com>
2025-05-19 15:07:29 +0300

    Update test_subject.cpp

--- tests/test_subject.cpp
+++ tests/test_subject.cpp
@@ -1,1 +1,7 @@
+#include <gtest/gtest.h>
+#include "../include/subject.h"
 
+TEST(SubjectTest, PureVirtual) {
+    EXPECT_TRUE(std::is_abstract<Subject>::value);
+    EXPECT_TRUE(std::is_polymorphic<Subject>::value);
+}
