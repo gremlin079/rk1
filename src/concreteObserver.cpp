622f23b6f8af693fdfc1bf21b3f78a8690775abe
gremlin079 <sashdmitriev2006@gmail.com>
2025-05-19 15:46:34 +0300

    Update concreteObserver.cpp

--- src/concreteObserver.cpp
+++ src/concreteObserver.cpp
@@ -1,1 +1,16 @@
+#include "ConcreteObserver.h"
+#include <iostream>
 
+ConcreteObserver::ConcreteObserver(const std::string& name, WeatherData* subject)
+    : name(name), subject(subject) {}
+
+void ConcreteObserver::update() {
+    std::cout << "Observer " << name << " received update: "
+              << "Temp = " << subject->getTemperature()
+              << ", Humidity = " << subject->getHumidity()
+              << ", Pressure = " << subject->getPressure() << std::endl;
+}
+
+std::string ConcreteObserver::getName() const {
+    return name;
+}
