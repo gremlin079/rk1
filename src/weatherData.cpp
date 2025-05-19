a3f67873c396c73013e33768d3f9f996a0f45bf1
gremlin079 <sashdmitriev2006@gmail.com>
2025-05-19 15:06:38 +0300

    Update weatherData.cpp

--- src/weatherData.cpp
+++ src/weatherData.cpp
@@ -1,1 +1,13 @@
+#include "weatherData.h"
 
+void WeatherData::registerObserver(Observer o) {
+    observers.push_back(o);
+}
+
+void WeatherData::removeObserver(Observer o) {
+
+}
+
+void WeatherData::notifyObservers() {
+
+}
