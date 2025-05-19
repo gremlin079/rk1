a7d3d4b50b3aa6944167ffea88345587d2d8a293
gremlin079 <sashdmitriev2006@gmail.com>
2025-05-19 15:45:55 +0300

    Update weatherData.cpp

--- src/weatherData.cpp
+++ src/weatherData.cpp
@@ -1,13 +1,39 @@
-#include "weatherData.h"
+#include "WeatherData.h"
 
-void WeatherData::registerObserver(Observer o) {
+void WeatherData::registerObserver(Observer* o) {
     observers.push_back(o);
 }
 
-void WeatherData::removeObserver(Observer o) {
-
+void WeatherData::removeObserver(Observer* o) {
+    for (auto it = observers.begin(); it != observers.end(); ++it) {
+        if (*it == o) {
+            observers.erase(it);
+            break;
+        }
+    }
 }
 
 void WeatherData::notifyObservers() {
+    for (Observer* observer : observers) {
+        observer->update();
+    }
+}
 
+void WeatherData::setMeasurements(float temp, float hum, float press) {
+    temperature = temp;
+    humidity = hum;
+    pressure = press;
+    notifyObservers();
+}
+
+float WeatherData::getTemperature() const {
+    return temperature;
+}
+
+float WeatherData::getHumidity() const {
+    return humidity;
+}
+
+float WeatherData::getPressure() const {
+    return pressure;
 }
